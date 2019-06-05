afb4b9ca0f76e4f8d8f222caff8f21df0a44c577
SunnyTong2019 <50933364+SunnyTong2019@users.noreply.github.com>
2019-06-05 10:43:59 -0700

    Added code for File Metadata Microservice

--- server.js
+++ server.js
@@ -3,7 +3,21 @@
 var express = require('express');
 var cors = require('cors');
 
+
+
 // require and use "multer"...
+var multer  = require('multer');
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '/tmp');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+var upload = multer({ storage: storage });
+
+
 
 var app = express();
 
@@ -17,6 +31,19 @@ app.get('/', function (req, res) {
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
 });
+
+
+
+//When I submit a form that includes a file upload, I will receive the file name and size in bytes within the JSON response
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next) => {
+  if (!req.file) 
+  { res.json('Please choose a file to upload.'); }
+  else
+  { res.json({file_name:req.file.originalname,file_size_in_bytes:req.file.size}); }
+  
+});
+
+
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
